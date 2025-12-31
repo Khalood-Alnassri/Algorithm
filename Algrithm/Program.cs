8ed937dc26ab5628993d6d05cca3ee390ca4a0a0
Khalood K. Al Nasiri <Khalood.kk@mem.gov.om>
2025-12-31 09:17:27 +0400

    task1 , task2 code

---
+++ Algrithm/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Algrithm
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the first number :");
+            float number1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the secound number :");
+            float number2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the theird number :");
+            float number3 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the four number :");
+            float number4 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the five number :");
+            float number5 = float.Parse(Console.ReadLine());
+
+            float result = number1 + number2 + number3 + number4 + number5;
+            Console.WriteLine("The result is :" +result);
+
+            float avg = result / 5;
+            Console.WriteLine("The avrage is :" + avg);
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The Number :");
+            float number = float.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
